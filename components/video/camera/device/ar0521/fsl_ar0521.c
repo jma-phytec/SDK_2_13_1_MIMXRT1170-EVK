2f46b0015f8d75a387819343d7c84c7ce43cc3e9
John Ma <110064859+jma-phytec@users.noreply.github.com>
2023-03-31 13:54:19 -0700

    dummy commit

--- components/video/camera/device/ar0521/fsl_ar0521.c
+++ components/video/camera/device/ar0521/fsl_ar0521.c
@@ -85,6 +85,225 @@ status_t AR0521_InitExt(camera_device_handle_t *handle, const camera_config_t *c
  * Variables
  ******************************************************************************/
 static const ar0521_reg_val_t ar0521InitParam[] = {
+		{ 0x3d00, 0x043e },
+		{ 0x3d02, 0x4760 },
+		{ 0x3d04, 0xffff },
+		{ 0x3d06, 0xffff },
+		{ 0x3d08, 0x8000 },
+		{ 0x3d0a, 0x0510 },
+		{ 0x3d0c, 0xaf08 },
+		{ 0x3d0e, 0x0252 },
+		{ 0x3d10, 0x486f },
+		{ 0x3d12, 0x5d5d },
+		{ 0x3d14, 0x8056 },
+		{ 0x3d16, 0x8313 },
+		{ 0x3d18, 0x0087 },
+		{ 0x3d1a, 0x6a48 },
+		{ 0x3d1c, 0x6982 },
+		{ 0x3d1e, 0x0280 },
+		{ 0x3d20, 0x8359 },
+		{ 0x3d22, 0x8d02 },
+		{ 0x3d24, 0x8020 },
+		{ 0x3d26, 0x4882 },
+		{ 0x3d28, 0x4269 },
+		{ 0x3d2a, 0x6a95 },
+		{ 0x3d2c, 0x5988 },
+		{ 0x3d2e, 0x5a83 },
+		{ 0x3d30, 0x5885 },
+		{ 0x3d32, 0x6280 },
+		{ 0x3d34, 0x6289 },
+		{ 0x3d36, 0x6097 },
+		{ 0x3d38, 0x5782 },
+		{ 0x3d3a, 0x605c },
+		{ 0x3d3c, 0xbf18 },
+		{ 0x3d3e, 0x0961 },
+		{ 0x3d40, 0x5080 },
+		{ 0x3d42, 0x2090 },
+		{ 0x3d44, 0x4390 },
+		{ 0x3d46, 0x4382 },
+		{ 0x3d48, 0x5f8a },
+		{ 0x3d4a, 0x5d5d },
+		{ 0x3d4c, 0x9c63 },
+		{ 0x3d4e, 0x8063 },
+		{ 0x3d50, 0xa960 },
+		{ 0x3d52, 0x9757 },
+		{ 0x3d54, 0x8260 },
+		{ 0x3d56, 0x5cff },
+		{ 0x3d58, 0xbf10 },
+		{ 0x3d5a, 0x1681 },
+		{ 0x3d5c, 0x0802 },
+		{ 0x3d5e, 0x8000 },
+		{ 0x3d60, 0x141c },
+		{ 0x3d62, 0x6000 },
+		{ 0x3d64, 0x6022 },
+		{ 0x3d66, 0x4d80 },
+		{ 0x3d68, 0x5c97 },
+		{ 0x3d6a, 0x6a69 },
+		{ 0x3d6c, 0xac6f },
+		{ 0x3d6e, 0x4645 },
+		{ 0x3d70, 0x4400 },
+		{ 0x3d72, 0x0513 },
+		{ 0x3d74, 0x8069 },
+		{ 0x3d76, 0x6ac6 },
+		{ 0x3d78, 0x5f95 },
+		{ 0x3d7a, 0x5f70 },
+		{ 0x3d7c, 0x8040 },
+		{ 0x3d7e, 0x4a81 },
+		{ 0x3d80, 0x0300 },
+		{ 0x3d82, 0xe703 },
+		{ 0x3d84, 0x0088 },
+		{ 0x3d86, 0x4a83 },
+		{ 0x3d88, 0x40ff },
+		{ 0x3d8a, 0xffff },
+		{ 0x3d8c, 0xfd70 },
+		{ 0x3d8e, 0x8040 },
+		{ 0x3d90, 0x4a85 },
+		{ 0x3d92, 0x4fa8 },
+		{ 0x3d94, 0x4f8c },
+		{ 0x3d96, 0x0070 },
+		{ 0x3d98, 0xbe47 },
+		{ 0x3d9a, 0x8847 },
+		{ 0x3d9c, 0xbc78 },
+		{ 0x3d9e, 0x6b89 },
+		{ 0x3da0, 0x6a80 },
+		{ 0x3da2, 0x6986 },
+		{ 0x3da4, 0x6b8e },
+		{ 0x3da6, 0x6b80 },
+		{ 0x3da8, 0x6980 },
+		{ 0x3daa, 0x6a88 },
+		{ 0x3dac, 0x7c9f },
+		{ 0x3dae, 0x866b },
+		{ 0x3db0, 0x8765 },
+		{ 0x3db2, 0x46ff },
+		{ 0x3db4, 0xe365 },
+		{ 0x3db6, 0xa679 },
+		{ 0x3db8, 0x4a40 },
+		{ 0x3dba, 0x4580 },
+		{ 0x3dbc, 0x44bc },
+		{ 0x3dbe, 0x7000 },
+		{ 0x3dc0, 0x8040 },
+		{ 0x3dc2, 0x0802 },
+		{ 0x3dc4, 0x10ef },
+		{ 0x3dc6, 0x0104 },
+		{ 0x3dc8, 0x3860 },
+		{ 0x3dca, 0x5d5d },
+		{ 0x3dcc, 0x5682 },
+		{ 0x3dce, 0x1300 },
+		{ 0x3dd0, 0x8648 },
+		{ 0x3dd2, 0x8202 },
+		{ 0x3dd4, 0x8082 },
+		{ 0x3dd6, 0x598a },
+		{ 0x3dd8, 0x0280 },
+		{ 0x3dda, 0x2048 },
+		{ 0x3ddc, 0x3060 },
+		{ 0x3dde, 0x8042 },
+		{ 0x3de0, 0x9259 },
+		{ 0x3de2, 0x865a },
+		{ 0x3de4, 0x8258 },
+		{ 0x3de6, 0x8562 },
+		{ 0x3de8, 0x8062 },
+		{ 0x3dea, 0x8560 },
+		{ 0x3dec, 0x9257 },
+		{ 0x3dee, 0x8221 },
+		{ 0x3df0, 0x10ff },
+		{ 0x3df2, 0xb757 },
+		{ 0x3df4, 0x9361 },
+		{ 0x3df6, 0x1019 },
+		{ 0x3df8, 0x8020 },
+		{ 0x3dfa, 0x9043 },
+		{ 0x3dfc, 0x8b43 },
+		{ 0x3dfe, 0x875f },
+		{ 0x3e00, 0x835d },
+		{ 0x3e02, 0x805d },
+		{ 0x3e04, 0x8163 },
+		{ 0x3e06, 0x8063 },
+		{ 0x3e08, 0xa060 },
+		{ 0x3e0a, 0x9157 },
+		{ 0x3e0c, 0x8260 },
+		{ 0x3e0e, 0x5cff },
+		{ 0x3e10, 0xffff },
+		{ 0x3e12, 0xffe5 },
+		{ 0x3e14, 0x1016 },
+		{ 0x3e16, 0x2048 },
+		{ 0x3e18, 0x0802 },
+		{ 0x3e1a, 0x1c60 },
+		{ 0x3e1c, 0x0014 },
+		{ 0x3e1e, 0x0060 },
+		{ 0x3e20, 0x2205 },
+		{ 0x3e22, 0x8120 },
+		{ 0x3e24, 0x908f },
+		{ 0x3e26, 0x6a80 },
+		{ 0x3e28, 0x6982 },
+		{ 0x3e2a, 0x5f9f },
+		{ 0x3e2c, 0x6f46 },
+		{ 0x3e2e, 0x4544 },
+		{ 0x3e30, 0x0005 },
+		{ 0x3e32, 0x8013 },
+		{ 0x3e34, 0x8069 },
+		{ 0x3e36, 0x6a80 },
+		{ 0x3e38, 0x7000 },
+		{ 0x3e3a, 0x0000 },
+		{ 0x3e3c, 0x0000 },
+		{ 0x3e3e, 0x0000 },
+		{ 0x3e40, 0x0000 },
+		{ 0x3e42, 0x0000 },
+		{ 0x3e44, 0x0000 },
+		{ 0x3e46, 0x0000 },
+		{ 0x3e48, 0x0000 },
+		{ 0x3e4a, 0x0000 },
+		{ 0x3e4c, 0x0000 },
+		{ 0x3e4e, 0x0000 },
+		{ 0x3e50, 0x0000 },
+		{ 0x3e52, 0x0000 },
+		{ 0x3e54, 0x0000 },
+		{ 0x3e56, 0x0000 },
+		{ 0x3e58, 0x0000 },
+		{ 0x3e5a, 0x0000 },
+		{ 0x3e5c, 0x0000 },
+		{ 0x3e5e, 0x0000 },
+		{ 0x3e60, 0x0000 },
+		{ 0x3e62, 0x0000 },
+		{ 0x3e64, 0x0000 },
+		{ 0x3e66, 0x0000 },
+		{ 0x3e68, 0x0000 },
+		{ 0x3e6a, 0x0000 },
+		{ 0x3e6c, 0x0000 },
+		{ 0x3e6e, 0x0000 },
+		{ 0x3e70, 0x0000 },
+		{ 0x3e72, 0x0000 },
+		{ 0x3e74, 0x0000 },
+		{ 0x3e76, 0x0000 },
+		{ 0x3e78, 0x0000 },
+		{ 0x3e7a, 0x0000 },
+		{ 0x3e7c, 0x0000 },
+		{ 0x3e7e, 0x0000 },
+		{ 0x3e80, 0x0000 },
+		{ 0x3e82, 0x0000 },
+		{ 0x3e84, 0x0000 },
+		{ 0x3e86, 0x0000 },
+		{ 0x3e88, 0x0000 },
+		{ 0x3e8a, 0x0000 },
+		{ 0x3e8c, 0x0000 },
+		{ 0x3e8e, 0x0000 },
+		{ 0x3e90, 0x0000 },
+		{ 0x3e92, 0x0000 },
+		{ 0x3e94, 0x0000 },
+		{ 0x3e96, 0x0000 },
+		{ 0x3e98, 0x0000 },
+		{ 0x3e9a, 0x0000 },
+		{ 0x3e9c, 0x0000 },
+		{ 0x3e9e, 0x0000 },
+		{ 0x3ea0, 0x0000 },
+		{ 0x3ea2, 0x0000 },
+		{ 0x3ea4, 0x0000 },
+		{ 0x3ea6, 0x0000 },
+		{ 0x3ea8, 0x0000 },
+		{ 0x3eaa, 0x0000 },
+		{ 0x3eac, 0x0000 },
+		{ 0x3eae, 0x0000 },
+		{ 0x3eb0, 0x0000 },
+		{ 0x3eb2, 0x0000 },
+		{ 0x3eb4, 0x0000 },
 };
 
 const camera_device_operations_t ar0521_ops = {
@@ -95,6 +314,9 @@ const camera_device_operations_t ar0521_ops = {
     .control  = AR0521_Control,
     .init_ext = AR0521_InitExt,
 };
+
+struct ar0521 ar0521_sensor;
+struct ar0521 *sensor = &ar0521_sensor;
 
 /*******************************************************************************
  * Code
@@ -174,6 +396,14 @@ static status_t AR0521_CheckChipID(camera_device_handle_t *handle)
 #ifdef AR0521_DEBUG
 	PRINTF("Device ID: 0x%04x customer rev: 0x%04x\r\n", model_id, customer_rev);
 #endif
+	if (sensor->model == AR0521_MODEL_UNKNOWN)
+	{
+		if (customer_rev & BIT(4))
+			sensor->model = AR0521_MODEL_COLOR;
+		else
+			sensor->model = AR0521_MODEL_MONOCHROME;
+	}
+
 	return kStatus_Success;
 }
 
@@ -200,6 +430,206 @@ static status_t AR0521_SoftwareReset(camera_device_handle_t *handle)
 	return AR0521_ModifyReg16(handle, AR0521_RESET_REGISTER, (uint8_t)BIT_RESET, (uint8_t)1U << 0);
 }
 
+static const struct ar0521_format ar0521_mono_formats[] = {
+	{
+		.code	= MEDIA_BUS_FMT_Y8_1X8,
+		.bpp	= 8,
+	}, {
+		.code	= MEDIA_BUS_FMT_Y10_1X10,
+		.bpp	= 10,
+	}, {
+		.code	= MEDIA_BUS_FMT_Y12_1X12,
+		.bpp	= 12,
+	},
+};
+
+static const struct ar0521_format ar0521_col_formats[] = {
+	{
+		.code	= MEDIA_BUS_FMT_SGRBG8_1X8,
+		.bpp	= 8,
+	}, {
+		.code	= MEDIA_BUS_FMT_SGRBG10_1X10,
+		.bpp	= 10,
+	}, {
+		.code	= MEDIA_BUS_FMT_SGRBG12_1X12,
+		.bpp	= 12,
+	},
+};
+
+static void ar0521_set_defaults(void)
+{
+	sensor->limits = (struct ar0521_sensor_limits) {
+					/* min		max	 */
+		.x 			= {0        ,	2603      },
+		.y 			= {0        ,	1955      },
+		.hlen 			= {3080     ,	65532     },
+		.vlen 			= {48       ,	65535     },
+		.hblank 		= {240      ,	65535     },
+		.vblank 		= {28       ,	65535     },
+		.ext_clk 		= {5000000  ,	64000000  },
+	};
+
+	sensor->crop.left = 4;
+	sensor->crop.top = 4;
+	sensor->crop.width = AR0521_DEF_WIDTH;
+	sensor->crop.height = AR0521_DEF_HEIGHT;
+
+	sensor->fmt.width = AR0521_DEF_WIDTH;
+	sensor->fmt.height = AR0521_DEF_HEIGHT;
+	sensor->fmt.field = V4L2_FIELD_NONE;
+	sensor->fmt.colorspace = V4L2_COLORSPACE_RAW;
+
+	if (sensor->model == AR0521_MODEL_MONOCHROME) {
+		sensor->formats = ar0521_mono_formats;
+		sensor->num_fmts = ARRAY_SIZE(ar0521_mono_formats);
+	} else {
+		sensor->formats = ar0521_col_formats;
+		sensor->num_fmts = ARRAY_SIZE(ar0521_col_formats);
+	}
+
+	sensor->fmt.code = sensor->formats[sensor->num_fmts - 1].code;
+	sensor->bpp = sensor->formats[sensor->num_fmts - 1].bpp;
+
+	sensor->w_scale = 1;
+	sensor->h_scale = 1;
+	sensor->hblank = sensor->limits.hblank.min;
+	sensor->vblank = sensor->limits.vblank.min;
+	sensor->hlen = sensor->limits.hlen.min;
+	sensor->vlen = sensor->fmt.height + sensor->vblank;
+	sensor->gains.red = 1000;
+	sensor->gains.greenr = 1000;
+	sensor->gains.greenb = 1000;
+	sensor->gains.blue = 1000;
+	sensor->gains.min_ref = 1000;
+
+	return;
+}
+
+static int ar0521_mipi_enter_lp11(struct ar0521 *sensor)
+{
+	int ret;
+
+	ret = ar0521_write(sensor, AR0521_SERIAL_TEST,
+			   AR0521_TEST_MODE_LP11 |
+			   AR0521_TEST_LANE_0 | AR0521_TEST_LANE_1 |
+			   AR0521_TEST_LANE_2 | AR0521_TEST_LANE_3);
+
+	ret = ar0521_set_bits(sensor, AR0521_SER_CTRL_STAT,
+			      BIT_FRAMER_TEST_MODE);
+
+	ret = ar0521_update_bits(sensor, AR0521_RESET_REGISTER,
+				 BIT_STREAM | BIT_SMIA_SER_DIS,
+				 BIT_STREAM);
+	return ret;
+}
+
+static int ar0521_enter_standby(struct ar0521 *sensor)
+{
+	uint16_t timeout = 1000;
+	int ret;
+	uint16_t val;
+
+	ret = ar0521_clear_bits(sensor, AR0521_RESET_REGISTER,
+				BIT_STREAM);
+
+	while (timeout) {
+		ar0521_read(sensor, AR0521_FRAME_STATUS, &val);
+
+		if (val & BIT_STANDBY_STATUS) {
+			break;
+		}
+
+		timeout--;
+
+		}
+	}
+
+	AR0521_DelayMs(5);
+
+	ar0521_set_bits(sensor, AR0521_RESET_REGISTER, BIT_SMIA_SER_DIS);
+
+	ar0521_clear_bits(sensor, AR0521_SER_CTRL_STAT,
+				BIT_FRAMER_TEST_MODE);
+
+	return 0;
+}
+
+static int ar0521_s_power(int on)
+{
+	int ret = 0;
+
+	if(on)
+	{
+		/* Enable MIPI LP-11 test mode as required by e.g. i.MX 6 */
+		if(!sensor->is_streaming)
+		{
+			ret = ar0521_mipi_enter_lp11(sensor);
+		}
+	}
+	else
+	{
+		ar0521_enter_standby(sensor);
+	}
+
+	return ret;
+}
+
+static int ar0521_stream_on(struct ar0521 *sensor)
+{
+	int ret;
+
+	ret = ar0521_enter_standby(sensor);
+
+	ret = ar0521_config_pll(sensor);
+
+	ret = ar0521_config_frame(sensor);
+
+	ret = ar0521_config_mipi(sensor);
+
+	sensor->is_streaming = true;
+	return 0;
+}
+
+static int ar0521_stream_off(struct ar0521 *sensor)
+{
+	int ret;
+
+	if (sensor->trigger) {
+		ret = ar0521_unset_trigger(sensor);
+	}
+
+	ret = ar0521_enter_standby(sensor);
+
+	sensor->is_streaming = false;
+	return ret;
+}
+
+
+static int ar0521_s_stream(int enable)
+{
+	int ret = 0;
+
+	if(enable && sensor->is_streaming)
+	{
+		return -1;
+	}
+
+	if(!enable && !sensor->is_streaming)
+		return 0;
+
+	if(enable)
+	{
+		ret = ar0521_stream_on(sensor);
+	}
+	else
+	{
+		ret = ar0521_stream_off(sensor);
+	}
+
+	return ret;
+}
+
+
 status_t AR0521_Init(camera_device_handle_t *handle, const camera_config_t *config)
 {
     status_t status;
@@ -214,13 +644,15 @@ status_t AR0521_Init(camera_device_handle_t *handle, const camera_config_t *conf
     /* Delay 5ms. */
     AR0521_DelayMs(5);
 
+	ar0521_set_defaults();
+
+	//ar0521_init_sequencer(sensor);
     /* Initialize. */
     status = AR0521_LoadRegVal(handle, ar0521InitParam, ARRAY_SIZE(ar0521InitParam));
     if (kStatus_Success != status)
     {
         return status;
     }
-
 
     return kStatus_Success;
 }
--- components/video/camera/device/ar0521/fsl_ar0521.h
+++ components/video/camera/device/ar0521/fsl_ar0521.h
@@ -27,7 +27,90 @@
 
 #define AR0521_CHIP_ID			0x0457
 #define	AR0521_CUSTOMER_REV			0x31fe
+#define AR0521_DEF_WIDTH		2592
+#define AR0521_DEF_HEIGHT		1944
 
+
+/* YUV (including grey) - next is	0x202e */
+#define MEDIA_BUS_FMT_Y8_1X8			0x2001
+#define MEDIA_BUS_FMT_UV8_1X8			0x2015
+#define MEDIA_BUS_FMT_UYVY8_1_5X8		0x2002
+#define MEDIA_BUS_FMT_VYUY8_1_5X8		0x2003
+#define MEDIA_BUS_FMT_YUYV8_1_5X8		0x2004
+#define MEDIA_BUS_FMT_YVYU8_1_5X8		0x2005
+#define MEDIA_BUS_FMT_UYVY8_2X8			0x2006
+#define MEDIA_BUS_FMT_VYUY8_2X8			0x2007
+#define MEDIA_BUS_FMT_YUYV8_2X8			0x2008
+#define MEDIA_BUS_FMT_YVYU8_2X8			0x2009
+#define MEDIA_BUS_FMT_Y10_1X10			0x200a
+#define MEDIA_BUS_FMT_Y10_2X8_PADHI_LE		0x202c
+#define MEDIA_BUS_FMT_UYVY10_2X10		0x2018
+#define MEDIA_BUS_FMT_VYUY10_2X10		0x2019
+#define MEDIA_BUS_FMT_YUYV10_2X10		0x200b
+#define MEDIA_BUS_FMT_YVYU10_2X10		0x200c
+#define MEDIA_BUS_FMT_Y12_1X12			0x2013
+#define MEDIA_BUS_FMT_UYVY12_2X12		0x201c
+#define MEDIA_BUS_FMT_VYUY12_2X12		0x201d
+#define MEDIA_BUS_FMT_YUYV12_2X12		0x201e
+#define MEDIA_BUS_FMT_YVYU12_2X12		0x201f
+#define MEDIA_BUS_FMT_Y14_1X14			0x202d
+#define MEDIA_BUS_FMT_UYVY8_1X16		0x200f
+#define MEDIA_BUS_FMT_VYUY8_1X16		0x2010
+#define MEDIA_BUS_FMT_YUYV8_1X16		0x2011
+#define MEDIA_BUS_FMT_YVYU8_1X16		0x2012
+#define MEDIA_BUS_FMT_YDYUYDYV8_1X16		0x2014
+#define MEDIA_BUS_FMT_UYVY10_1X20		0x201a
+#define MEDIA_BUS_FMT_VYUY10_1X20		0x201b
+#define MEDIA_BUS_FMT_YUYV10_1X20		0x200d
+#define MEDIA_BUS_FMT_YVYU10_1X20		0x200e
+#define MEDIA_BUS_FMT_VUY8_1X24			0x2024
+#define MEDIA_BUS_FMT_YUV8_1X24			0x2025
+#define MEDIA_BUS_FMT_UYYVYY8_0_5X24		0x2026
+#define MEDIA_BUS_FMT_UYVY12_1X24		0x2020
+#define MEDIA_BUS_FMT_VYUY12_1X24		0x2021
+#define MEDIA_BUS_FMT_YUYV12_1X24		0x2022
+#define MEDIA_BUS_FMT_YVYU12_1X24		0x2023
+#define MEDIA_BUS_FMT_YUV10_1X30		0x2016
+#define MEDIA_BUS_FMT_UYYVYY10_0_5X30		0x2027
+#define MEDIA_BUS_FMT_AYUV8_1X32		0x2017
+#define MEDIA_BUS_FMT_UYYVYY12_0_5X36		0x2028
+#define MEDIA_BUS_FMT_YUV12_1X36		0x2029
+#define MEDIA_BUS_FMT_YUV16_1X48		0x202a
+#define MEDIA_BUS_FMT_UYYVYY16_0_5X48		0x202b
+
+/* Bayer - next is	0x3021 */
+#define MEDIA_BUS_FMT_SBGGR8_1X8		0x3001
+#define MEDIA_BUS_FMT_SGBRG8_1X8		0x3013
+#define MEDIA_BUS_FMT_SGRBG8_1X8		0x3002
+#define MEDIA_BUS_FMT_SRGGB8_1X8		0x3014
+#define MEDIA_BUS_FMT_SBGGR10_ALAW8_1X8		0x3015
+#define MEDIA_BUS_FMT_SGBRG10_ALAW8_1X8		0x3016
+#define MEDIA_BUS_FMT_SGRBG10_ALAW8_1X8		0x3017
+#define MEDIA_BUS_FMT_SRGGB10_ALAW8_1X8		0x3018
+#define MEDIA_BUS_FMT_SBGGR10_DPCM8_1X8		0x300b
+#define MEDIA_BUS_FMT_SGBRG10_DPCM8_1X8		0x300c
+#define MEDIA_BUS_FMT_SGRBG10_DPCM8_1X8		0x3009
+#define MEDIA_BUS_FMT_SRGGB10_DPCM8_1X8		0x300d
+#define MEDIA_BUS_FMT_SBGGR10_2X8_PADHI_BE	0x3003
+#define MEDIA_BUS_FMT_SBGGR10_2X8_PADHI_LE	0x3004
+#define MEDIA_BUS_FMT_SBGGR10_2X8_PADLO_BE	0x3005
+#define MEDIA_BUS_FMT_SBGGR10_2X8_PADLO_LE	0x3006
+#define MEDIA_BUS_FMT_SBGGR10_1X10		0x3007
+#define MEDIA_BUS_FMT_SGBRG10_1X10		0x300e
+#define MEDIA_BUS_FMT_SGRBG10_1X10		0x300a
+#define MEDIA_BUS_FMT_SRGGB10_1X10		0x300f
+#define MEDIA_BUS_FMT_SBGGR12_1X12		0x3008
+#define MEDIA_BUS_FMT_SGBRG12_1X12		0x3010
+#define MEDIA_BUS_FMT_SGRBG12_1X12		0x3011
+#define MEDIA_BUS_FMT_SRGGB12_1X12		0x3012
+#define MEDIA_BUS_FMT_SBGGR14_1X14		0x3019
+#define MEDIA_BUS_FMT_SGBRG14_1X14		0x301a
+#define MEDIA_BUS_FMT_SGRBG14_1X14		0x301b
+#define MEDIA_BUS_FMT_SRGGB14_1X14		0x301c
+#define MEDIA_BUS_FMT_SBGGR16_1X16		0x301d
+#define MEDIA_BUS_FMT_SGBRG16_1X16		0x301e
+#define MEDIA_BUS_FMT_SGRBG16_1X16		0x301f
+#define MEDIA_BUS_FMT_SRGGB16_1X16		0x3020
 
 /*!
  * @brief AR0521 resource.
@@ -43,6 +126,157 @@ typedef struct _ar0521_resource
 
 /*! @brief AR0521 operation functions. */
 extern const camera_device_operations_t ar0521_ops;
+
+enum ar0521_model {
+	AR0521_MODEL_UNKNOWN,
+	AR0521_MODEL_COLOR,
+	AR0521_MODEL_MONOCHROME,
+};
+
+enum v4l2_field {
+	V4L2_FIELD_ANY           = 0, /* driver can choose from none,
+					 top, bottom, interlaced
+					 depending on whatever it thinks
+					 is approximate ... */
+	V4L2_FIELD_NONE          = 1, /* this device has no fields ... */
+	V4L2_FIELD_TOP           = 2, /* top field only */
+	V4L2_FIELD_BOTTOM        = 3, /* bottom field only */
+	V4L2_FIELD_INTERLACED    = 4, /* both fields interlaced */
+	V4L2_FIELD_SEQ_TB        = 5, /* both fields sequential into one
+					 buffer, top-bottom order */
+	V4L2_FIELD_SEQ_BT        = 6, /* same as above + bottom-top order */
+	V4L2_FIELD_ALTERNATE     = 7, /* both fields alternating into
+					 separate buffers */
+	V4L2_FIELD_INTERLACED_TB = 8, /* both fields interlaced, top field
+					 first and the top field is
+					 transmitted first */
+	V4L2_FIELD_INTERLACED_BT = 9, /* both fields interlaced, top field
+					 first and the bottom field is
+					 transmitted first */
+};
+
+enum v4l2_colorspace {
+	/*
+	 * Default colorspace, i.e. let the driver figure it out.
+	 * Can only be used with video capture.
+	 */
+	V4L2_COLORSPACE_DEFAULT       = 0,
+
+	/* SMPTE 170M: used for broadcast NTSC/PAL SDTV */
+	V4L2_COLORSPACE_SMPTE170M     = 1,
+
+	/* Obsolete pre-1998 SMPTE 240M HDTV standard, superseded by Rec 709 */
+	V4L2_COLORSPACE_SMPTE240M     = 2,
+
+	/* Rec.709: used for HDTV */
+	V4L2_COLORSPACE_REC709        = 3,
+
+	/*
+	 * Deprecated, do not use. No driver will ever return this. This was
+	 * based on a misunderstanding of the bt878 datasheet.
+	 */
+	V4L2_COLORSPACE_BT878         = 4,
+
+	/*
+	 * NTSC 1953 colorspace. This only makes sense when dealing with
+	 * really, really old NTSC recordings. Superseded by SMPTE 170M.
+	 */
+	V4L2_COLORSPACE_470_SYSTEM_M  = 5,
+
+	/*
+	 * EBU Tech 3213 PAL/SECAM colorspace.
+	 */
+	V4L2_COLORSPACE_470_SYSTEM_BG = 6,
+
+	/*
+	 * Effectively shorthand for V4L2_COLORSPACE_SRGB, V4L2_YCBCR_ENC_601
+	 * and V4L2_QUANTIZATION_FULL_RANGE. To be used for (Motion-)JPEG.
+	 */
+	V4L2_COLORSPACE_JPEG          = 7,
+
+	/* For RGB colorspaces such as produces by most webcams. */
+	V4L2_COLORSPACE_SRGB          = 8,
+
+	/* opRGB colorspace */
+	V4L2_COLORSPACE_OPRGB         = 9,
+
+	/* BT.2020 colorspace, used for UHDTV. */
+	V4L2_COLORSPACE_BT2020        = 10,
+
+	/* Raw colorspace: for RAW unprocessed images */
+	V4L2_COLORSPACE_RAW           = 11,
+
+	/* DCI-P3 colorspace, used by cinema projectors */
+	V4L2_COLORSPACE_DCI_P3        = 12,
+
+	/* Generic film (colour filters using Illuminant C) */
+	V4L2_COLORSPACE_GENERIC_FILM  = 13,
+
+	/* SMPTE ST 428-1 */
+	V4L2_COLORSPACE_ST428         = 14,
+};
+
+
+struct limit_range {
+	uint32_t min;
+	uint32_t max;
+};
+
+struct ar0521_sensor_limits {
+	struct limit_range x;
+	struct limit_range y;
+	struct limit_range hlen;
+	struct limit_range vlen;
+	struct limit_range hblank;
+	struct limit_range vblank;
+	struct limit_range ext_clk;
+};
+
+struct v4l2_mbus_framefmt {
+	uint32_t	width;
+	uint32_t	height;
+	uint32_t	code;
+	uint32_t	field;
+	uint32_t	colorspace;
+};
+
+struct v4l2_rect {
+	uint32_t	left;
+	uint32_t	top;
+	uint32_t	width;
+	uint32_t	height;
+};
+
+struct ar0521_gains {
+	uint32_t	red;
+	uint32_t	greenb;
+	uint32_t	greenr;
+	uint32_t	blue;
+	uint32_t	min_ref;
+};
+
+struct ar0521_format {
+	uint32_t	code;
+	uint32_t	bpp;
+};
+
+struct ar0521 {
+	struct v4l2_mbus_framefmt fmt;
+	struct v4l2_rect crop;
+	struct ar0521_sensor_limits limits;
+	enum ar0521_model model;
+	const struct ar0521_format *formats;
+	uint32_t	num_fmts;
+	uint32_t	bpp;
+	uint32_t	w_scale;
+	uint32_t	h_scale;
+	uint32_t	vblank;
+	uint32_t	hblank;
+	uint32_t	hlen;
+	uint32_t	vlen;
+	struct ar0521_gains gains;
+	bool is_streaming;
+};
 
 /*******************************************************************************
  * API
